2ee425f09e45c8f32c1b77776ba176da8eb4bbf1
Chris Gemignani <chris.gemignani@juiceanalytics.com>
2016-04-20 09:55:37 -0500

    Use automatic_filter and custom_filter rather than global_filter and local_filter
    Build the basic response in the DataService.run()

--- stacks/census/censusv2service.py
+++ stacks/census/censusv2service.py
@@ -1,11 +1,8 @@
-from copy import deepcopy
+from sqlalchemy import Column, String, ForeignKey, select, join
 
-from sqlalchemy import Column, String, Integer, Float, case, ForeignKey, select, join, Table, or_
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from dataservices.servicebase import *
 from dataservices.recipe import *
 from dataservices.redshift_connectionbase import *
+from dataservices.servicebase import *
 
 # -------------
 # Connect to the database
@@ -20,12 +17,61 @@ engine_green = redshift_create_engine_green()
 
 Base = declarative_base()
 
+import gevent
+from gevent.queue import Queue
+from gevent.pool import Pool
+
+
+class RecipePool(object):
+    def __init__(self, recipes):
+        self.recipes = recipes
+        self.tasks = Queue()
+        self.pool = Pool(5)
+
+    def __query(self, recipe, name):
+        recipe.session = sessionmaker(bind=engine_green)()
+        result = recipe.render(name)
+        return result
+
+    def executor(self, number):
+        while not self.tasks.empty():
+            recipe, name = self.tasks.get()
+            return self.__query(recipe, name)
+
+    def overseer(self):
+        for recipe in self.recipes:
+            self.tasks.put_nowait(recipe)
+
+    def run(self):
+        gevent.spawn(self.overseer).join()
+        workers_complete = self.pool.map(self.executor,
+                                         xrange(len(self.tasks)))
+        results = {}
+        for worker in workers_complete:
+            results[worker['name']] = worker
+        workers_complete = []
+        for recipe, name in self.recipes:
+            workers_complete.append(results[name])
+
+        return workers_complete
+
 
 # -------------
 # Step 1: Define the tables used in your application
 # -------------
 
+class FirstDict(dict):
+    """
+    A dictionary with an added `first(key, default)` accessor that
+    gets the first element in a list.
+    """
 
+    def first(self, key, default=None):
+        value = self.get(key, default)
+        if isinstance(value, (list, tuple)):
+            return value[0]
+        else:
+            return value
 
 
 class DivLookup(Base):
@@ -57,7 +103,8 @@ class StateFact(Base):
                       Column('standard_federal_region', String()),
                       Column('census_region', String()),
                       Column('census_region_name', String()),
-                      Column('census_division', String(), ForeignKey(DivLookup.census_division)),
+                      Column('census_division', String(),
+                             ForeignKey(DivLookup.census_division)),
                       Column('census_division_name', String()),
                       Column('circuit_court', String()),
                       schema='demo', extend_existing=True)
@@ -71,7 +118,8 @@ class Census(Base):
     Again, a primary key is defined but will not be used.
     """
     __table__ = Table('census', Base.metadata,
-                      Column('state', String(30), ForeignKey(StateFact.name), primary_key=True),
+                      Column('state', String(30), ForeignKey(StateFact.name),
+                             primary_key=True),
                       Column('sex', String(1)),
                       Column('age', Float()),
                       Column('pop2000', Float()),
@@ -82,7 +130,8 @@ class Census(Base):
 
 # Another approach is to create a joined view
 class CensusJoined(Base):
-    __table__ = select([Census, StateFact]).select_from(join(Census, StateFact, Census.state == StateFact.name)).alias()
+    __table__ = select([Census, StateFact]).select_from(
+        join(Census, StateFact, Census.state == StateFact.name)).alias()
 
 
 # -------------
@@ -97,23 +146,30 @@ class RecipeServiceBaseV3(object):
         """
     metric_shelf = {}
     dimension_shelf = {}
-    dimensions = {}
     filter_shelf = {}
-    global_filters = []
-    local_filters = []
-    global_filter_ids = ()
 
-    def __init__(self, request, config, slice_type=None, user_filters=None, stack_filters=None):
+    # A list of dimensions or metrics that will be automatically filtered
+    # Gathered into self.automatic_filters (dict)
+    automatic_filter_keys = ()
+    # A list of dimensions or metrics that will be gathered into
+    # self.custom_filters (dict)
+    custom_filter_keys = ()
+
+    def __init__(self, request, config, slice_type=None, user_filters=None,
+                 stack_filters=None):
         self.request = request
         self.config = config
         self.slice_type = slice_type
         self.user_filters = user_filters
         self.stack_filters = stack_filters
-        self.Session = sessionmaker(bind=engine_green)
-        self.filter_ingredients = []
-        self.filters = []
+        self.Session = sessionmaker()
+
+        # Metrics, dimensions and filters to apply automatically to recipes
         self.metrics = []
         self.dimensions = []
+        self.filters = []
+        self.automatic_filters = FirstDict()
+        self.custom_filters = FirstDict()
 
     @contextmanager
     def session_scope(self):
@@ -142,6 +198,16 @@ class RecipeServiceBaseV3(object):
 
         return Recipe(*args, **kwargs)
 
+    @property
+    def global_filters(self):
+        """ For backward compatability """
+        return self.automatic_filters
+
+    @property
+    def local_filters(self):
+        """ For backward compatability """
+        return self.custom_filters
+
     def build_request_params(self):
         """ Build a dictionary of query parameters to use as global filters.
         Also keep a reference to any metric supplied.
@@ -154,31 +220,39 @@ class RecipeServiceBaseV3(object):
             params = self.request.data
         else:
             params = {}
+
+        # Save the passed params
         self.params = params
-        # Build the global filters.
-        self.global_filters = {}
-        for id in self.global_filter_ids:
+
+        # Load the automatic filters
+        self.automatic_filters.clear()
+        for id in self.automatic_filter_keys:
             v = params.get(id, None)
             if v:
-                self.global_filters[id] = v
+                self.automatic_filters[id] = v
 
-        # self.global_filters = {k: [unquote_plus(_) for _ in v] for
-        #                        k, v in self.global_filters.iteritems()}
-        self.local_filters = {}
-        filters = []
+        # Load the custom filters
+        self.custom_filters.clear()
+        for id in self.custom_filter_keys:
+            v = params.get(id, None)
+            if v:
+                self.custom_filters[id] = v
 
-        for filter_ingredient in self.filter_ingredients:
-            if filter_ingredient in params:
-                filters.append(self.dimension_shelf[
-                    filter_ingredient].filter_values(params[filter_ingredient])
-                )
+        # for filter_ingredient in self.filter_ingredients:
+        #     if filter_ingredient in params:
+        #         filters.append(self.dimension_shelf[
+        #                            filter_ingredient].filter_values(
+        #             params[filter_ingredient])
+        #                        )
         self.metrics = params.get('metric', [])
-        self.dimensions = params.get('dimentions', [])
+        self.dimensions = params.get('dimensions', [])
 
     def apply_user_filters(self, query=None, table=None):
+        """ For recipe compatability, no longer used in jb3 """
         return query
 
     def apply_global_filters(self, query=None, table=None, limit_to=None):
+        """ For recipe compatability, no longer used in jb3 """
         return query
 
     def build_response(self):
@@ -186,7 +260,7 @@ class RecipeServiceBaseV3(object):
 
         Subclasses should override this to do the following
 
-        1) Build one or more queries
+        1) Evaluate local filters
         2) apply_user_filters on that query
         3) apply_global_filters on that query
         4) Apply local filters where needed
@@ -203,6 +277,11 @@ class RecipeServiceBaseV3(object):
     def run(self):
         """ Process everything
         """
+        self.response = {
+            'responses': [],
+            'version': '3'
+        }
+
         with self.session_scope():
             self.build_request_params()
             self.build_response()
@@ -211,45 +290,61 @@ class RecipeServiceBaseV3(object):
 class CensusService(RecipeServiceBaseV3):
     # Metrics are defined as an SQLAlchemy expression, and a label.
     metric_shelf = {
-        'pop2000': Metric(func.sum(Census.pop2000), label='Population 2000', format=".3s", singular="Population 2000",
-                          plural="cookies"),
-        'pop2008': Metric(func.sum(Census.pop2008), label='Population 2008', format=".3s"),
-        'popdiff': Metric(func.sum(Census.pop2008 - Census.pop2000), label='Population Growth'),
-        'avgage': Metric(func.sum(Census.pop2008 * Census.age) / func.sum(Census.pop2008), label='Average Age'),
+        'pop2000': Metric(func.sum(Census.pop2000), label='Population 2000',
+                          format=".3s", singular="Population 2000",
+                          plural="Population 2000"),
+        'pop2008': Metric(func.sum(Census.pop2008), singular='Population 2008',
+                          format=".3s"),
+        'popdiff': Metric(func.sum(Census.pop2008 - Census.pop2000),
+                          format=".3s",
+                          singular='Population Growth'),
+        'avgage': Metric(
+            func.sum(Census.pop2008 * Census.age) / func.sum(Census.pop2008),
+            singular='Average Age', format=".1f"),
         # A metric using a complex expression
-        'pctfemale': Metric(func.sum(case([(Census.sex == 'F', Census.pop2008)], else_=0)) / func.sum(Census.pop2008),
-                            label='% Female'),
+        'pctfemale': Metric(func.sum(
+            case([(Census.sex == 'F', Census.pop2008)], else_=0)) / func.sum(
+            Census.pop2008), format=".1%",
+                            singular='% Female'),
         # a metric using a formatter
-        'pctdiff': Metric(func.sum(Census.pop2008 - Census.pop2000) / func.sum(Census.pop2000),
-                          label='Population Pct Change',
-                          formatters=[lambda x: "Change is {0:0.1%} percent".format(x)]),
+        'pctdiff': Metric(func.sum(Census.pop2008 - Census.pop2000) / func.sum(
+            Census.pop2000),
+                          singular='Population Pct Change',
+                          formatters=[
+                              lambda x: "Change is {0:0.1%} percent".format(
+                                  x)]),
     }
 
     # Dimensions are ways to split the data.
     dimension_shelf = {
         # Simplest possible dimension, a SQLAlchemy expression and a label.
         'state': Dimension(Census.state, singular='State', plural='States'),
-        'first_letter_state': Dimension(func.substring(Census.state,1,1), label='State'),
+        'first_letter_state': Dimension(func.substring(Census.state, 1, 1),
+                                        label='State'),
         'age': Dimension(Census.age, singular='Age', plural='Ages'),
         'age_bands': Dimension(case([(Census.age < 21, 'Under 21'),
                                      (Census.age < 49, '21-49')
                                      ], else_='Other'), label='Age Bands'),
         # A dimension that requires a join, the join can be a relationship, or a table
-        'region': Dimension(StateFact.standard_federal_region, label='Region', join=StateFact),
+        'region': Dimension(StateFact.standard_federal_region, label='Region',
+                            join=StateFact),
         # A dimension that requires multiple joins
-        'divname': Dimension(DivLookup.name, label='Census DivisionNames', join=[DivLookup, StateFact]),
-        'circuit_court': Dimension(StateFact.circuit_court, label='Circuit Court', join=StateFact),
+        'divname': Dimension(DivLookup.name, label='Census DivisionNames',
+                             join=[DivLookup, StateFact]),
+        'circuit_court': Dimension(StateFact.circuit_court,
+                                   label='Circuit Court', join=StateFact),
 
         # This will use the lookup to get display values of "M" and "F"
-        'sex': LookupDimension(Census.sex, singular='Gender', plural='Genders', lookup={'M': 'Menfolk', "F": "Womenfolk"}),
+        'sex': LookupDimension(Census.sex, singular='Gender', plural='Genders',
+                               lookup={'M': 'Menfolk', "F": "Womenfolk"}),
         # Formatters apply functions to the response
-        'gender': Dimension(Census.sex, label='Sex', formatters=[lambda x: ord(x), lambda x: x + 100]),
+        'gender': Dimension(Census.sex, label='Sex',
+                            formatters=[lambda x: ord(x), lambda x: x + 100]),
     }
 
-    # Dimension order will be used for the global filters
-    global_filter_ids = ('sex', 'state',)
+    # Automatic filter keys are used for the global filters as well
+    automatic_filter_keys = ('sex', 'state',)
     default_metric = 'pop2000'
-    default_table = Census
 
     def __init__(self, *args, **kwargs):
         super(CensusService, self).__init__(*args, **kwargs)
@@ -261,27 +356,18 @@ class CensusService(RecipeServiceBaseV3):
 # -------------
 
 def generate_default_filter_service(base):
-
     class DefaultFilterService(base):
-
-            def build_response(self):
-                self.response = {
-                    'responses': []
-                }
-                for dim in self.global_filter_ids:
-                    recipe = self.recipe().metrics(
-                        self.default_metric).dimensions(dim)
-                    self.response['responses'].append(recipe.render())
+        def build_response(self):
+            for dim in self.automatic_filter_keys:
+                recipe = self.recipe().metrics(self.default_metric).dimensions(
+                    dim)
+                self.response['responses'].append(recipe.render())
 
 
 class FilterService(CensusService):
-
     def build_response(self):
         self.metrics = [self.default_metric]
-        self.response = {
-            'responses': []
-        }
-        for dim in self.global_filter_ids:
+        for dim in self.automatic_filter_keys:
             recipe = self.recipe().metrics(*self.metrics).dimensions(dim)
             self.response['responses'].append(recipe.render())
 
@@ -289,69 +375,24 @@ class FilterService(CensusService):
 class FirstChooserV3Service(CensusService):
     def build_response(self):
         self.metrics = ('pop2000', 'pop2008', 'popdiff', 'avgage', 'pctfemale')
-
         recipe = self.recipe().metrics(*self.metrics)
-        self.response = {
-            'responses': [recipe.render()]
-        }
+        self.response['responses'].append(recipe.render())
 
 
 class SecondChooserV3Service(CensusService):
     def build_response(self):
-        self.dimensions.append('sex')
+        self.dimensions = ('sex',)
         recipe = self.recipe().dimensions(
             *self.dimensions).metrics(*self.metrics)
+        self.response['responses'].append(recipe.render())
 
-        self.response = {
-            'responses': [recipe.render()]
-        }
-
-
-import gevent
-from gevent.queue import Queue
-from gevent.pool import Pool
-class RecipePool(object):
-    def __init__(self, recipes):
-        self.recipes = recipes
-        self.tasks = Queue()
-        self.pool = Pool(5)
-
-    def __query(self, recipe, name):
-        recipe.session = sessionmaker(bind=engine_green)()
-        result =  recipe.render(name)
-        return result
-
-    def executor(self, number):
-        while not self.tasks.empty():
-            recipe, name = self.tasks.get()
-            return self.__query(recipe, name)
-
-    def overseer(self):
-        for recipe in self.recipes:
-            self.tasks.put_nowait(recipe)
-
-    def run(self):
-        gevent.spawn(self.overseer).join()
-        workers_complete = self.pool.map(self.executor,
-                                         xrange(len(self.tasks)))
-        results = {}
-        for worker in workers_complete:
-            results[worker['name']] = worker
-        workers_complete = []
-        for recipe, name in self.recipes:
-            workers_complete.append(results[name])
-
-        return workers_complete
 
 class DistributionV3Service(CensusService):
     def build_response(self):
-        self.dimensions.extend(['age_bands', 'age'])
-        self.filter_ingredients = ['sex']
-
-
+        self.dimensions = ['age_bands', 'age']
         recipe1 = self.recipe().dimensions(*self.dimensions) \
             .metrics(*self.metrics).order_by(*self.dimensions).filters(
-                *self.filters)
+            *self.filters)
 
         self.dimensions = ['first_letter_state', 'state']
 
@@ -360,14 +401,4 @@ class DistributionV3Service(CensusService):
             .filters(*self.filters)
 
         results = RecipePool([(recipe1, 'Ages'), (recipe2, 'States')]).run()
-        self.response = {
-            'responses': results
-        }
-
-        # self.response = {
-        #     'responses': [self.recipe().dimensions('age_bands', 'age').metrics(metric).order_by('age_bands', 'age').filters(*filters).render()
-        #                   self.recipe().dimensions('first_letter_state', 'state').metrics(metric).order_by(
-        #                       'first_letter_state', 'state').filters(*filters).render()]
-        # }
-
-
+        self.response['responses'] = results
