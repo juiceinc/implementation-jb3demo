fa43aba4d0050c43c0c681b5a4827525e43ca127
Jason Myers <jason@jasonamyers.com>
2016-04-22 10:19:01 -0500

    Moving RecipeServiceBaseV3 out to it's own file
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- stacks/census/censusv2service.py
+++ stacks/census/censusv2service.py
@@ -1,12 +1,11 @@
 import time
 from sqlalchemy import Column, String, ForeignKey, select, join
-from dataservices.connectionbase import regions
 
 from dataservices.recipe import *
 from dataservices.redshift_connectionbase import *
 from dataservices.servicebase import *
 from dataservices.recipe_pool import RecipePool
-from dataservices import caching_query
+from dataservices.servicebasev3 import RecipeServiceBaseV3
 
 # -------------
 # Connect to the database
@@ -26,18 +25,6 @@ current_milli_time = lambda: int(round(time.time() * 1000))
 # Step 1: Define the tables used in your application
 # -------------
 
-class FirstDict(dict):
-    """
-    A dictionary with an added `first(key, default)` accessor that
-    gets the first element in a list.
-    """
-
-    def first(self, key, default=None):
-        value = self.get(key, default)
-        if isinstance(value, (list, tuple)):
-            return value[0]
-        else:
-            return value
 
 
 class DivLookup(Base):
@@ -105,153 +92,6 @@ class CensusJoined(Base):
 # on the dimension_shelf
 # -------------
 
-class RecipeServiceBaseV3(object):
-    """ A base class for Fruition Data Services using recipes
-
-        Subclasses should define a dimension_shelf and metric_shelf
-        """
-    metric_shelf = {}
-    dimension_shelf = {}
-    filter_shelf = {}
-
-    # A list of dimensions or metrics that will be automatically filtered
-    # Gathered into self.automatic_filters (dict)
-    automatic_filter_keys = ()
-    # A list of dimensions or metrics that will be gathered into
-    # self.custom_filters (dict)
-    custom_filter_keys = ()
-
-    def __init__(self, request, config, slice_type=None, user_filters=None,
-                 stack_filters=None):
-        self.request = request
-        self.config = config
-        self.slice_type = slice_type
-        self.user_filters = user_filters
-        self.stack_filters = stack_filters
-        self.Session = sessionmaker(query_cls=caching_query.query_callable(regions),
-                                    bind=engine)
-
-        # Metrics, dimensions and filters to apply automatically to recipes
-        self.metrics = []
-        self.dimensions = []
-        self.filters = []
-        self.automatic_filters = FirstDict()
-        self.custom_filters = FirstDict()
-
-    @contextmanager
-    def session_scope(self):
-        """ Provide a transactional scope around a series of operations.
-        """
-        self.session = self.Session()
-        try:
-            yield self.session
-            self.session.commit()
-        except:
-            self.session.rollback()
-            raise
-        finally:
-            self.session.close()
-
-    def recipe(self, *args, **kwargs):
-        args = list(args)
-
-        # Insert a reference to the service
-        args.insert(0, self)
-
-        # Use the default table if no table is passed and there is a
-        # default_table defined
-        if len(args) == 1 and hasattr(self, 'default_table'):
-            args.append(self.default_table)
-
-        return Recipe(*args, **kwargs)
-
-    @property
-    def global_filters(self):
-        """ For backward compatability """
-        return self.automatic_filters
-
-    @property
-    def local_filters(self):
-        """ For backward compatability """
-        return self.custom_filters
-
-    def build_request_params(self):
-        """ Build a dictionary of query parameters to use as global filters.
-        Also keep a reference to any metric supplied.
-
-        If a data service defines a list of local filter names
-        `self.local_filter_ids`, then it will create a dictionary of the local
-        filters along with their values in `self.local_filters`.
-        """
-        if self.request:
-            params = self.request.data
-        else:
-            params = {}
-
-        # Save the passed params
-        self.params = params
-
-        # Load the automatic filters
-        self.automatic_filters.clear()
-        for id in self.automatic_filter_keys:
-            v = params.get(id, None)
-            if v:
-                self.automatic_filters[id] = v
-
-        # Load the custom filters
-        self.custom_filters.clear()
-        for id in self.custom_filter_keys:
-            v = params.get(id, None)
-            if v:
-                self.custom_filters[id] = v
-
-        # for filter_ingredient in self.filter_ingredients:
-        #     if filter_ingredient in params:
-        #         filters.append(self.dimension_shelf[
-        #                            filter_ingredient].filter_values(
-        #             params[filter_ingredient])
-        #                        )
-        self.metrics = params.get('metric', [])
-        self.dimensions = params.get('dimensions', [])
-
-    def apply_user_filters(self, query=None, table=None):
-        """ For recipe compatability, no longer used in jb3 """
-        return query
-
-    def apply_global_filters(self, query=None, table=None, limit_to=None):
-        """ For recipe compatability, no longer used in jb3 """
-        return query
-
-    def build_response(self):
-        """ Build the response.
-
-        Subclasses should override this to do the following
-
-        1) Evaluate local filters
-        2) apply_user_filters on that query
-        3) apply_global_filters on that query
-        4) Apply local filters where needed
-        5) Format the result and return it
-
-        ----
-
-        Built a recipe/ mapper from props in recipe to props in response
-        a list of objects where each object has a group_by_type and id
-        or a list of list of objects
-        """
-        pass
-
-    def run(self):
-        """ Process everything
-        """
-        self.response = {
-            'responses': [],
-            'version': '3'
-        }
-
-        with self.session_scope():
-            self.build_request_params()
-            self.build_response()
 
 
 class CensusService(RecipeServiceBaseV3):
