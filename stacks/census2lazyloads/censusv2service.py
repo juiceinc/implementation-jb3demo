d42f4bac330218d3cf7e85d113285f7501cbb631
Chris Gemignani <chris.gemignani@juiceanalytics.com>
2022-07-28 09:02:37 -0400

    Black format and remove tests

--- stacks/census2lazyloads/censusv2service.py
+++ stacks/census2lazyloads/censusv2service.py
@@ -34,12 +34,15 @@ current_milli_time = lambda: int(round(time.time() * 1000))
 # -------------
 
 
-
 class DivLookup(Base):
-    __table__ = Table('state_census_div_lookup', Base.metadata,
-                      Column('census_division', String(), primary_key=True),
-                      Column('name', String()),
-                      schema='demo', extend_existing=True)
+    __table__ = Table(
+        "state_census_div_lookup",
+        Base.metadata,
+        Column("census_division", String(), primary_key=True),
+        Column("name", String()),
+        schema="demo",
+        extend_existing=True,
+    )
 
 
 class StateFact(Base):
@@ -49,26 +52,30 @@ class StateFact(Base):
 
     Again, a primary key is defined but will not be used.
     """
-    __table__ = Table('state_fact', Base.metadata,
-                      Column('id', String()),
-                      Column('name', String(), primary_key=True),
-                      Column('abbreviation', String()),
-                      Column('country', String()),
-                      Column('type', String()),
-                      Column('sort', String()),
-                      Column('status', String()),
-                      Column('occupied', String()),
-                      Column('notes', String()),
-                      Column('fips_state', String()),
-                      Column('assoc_press', String()),
-                      Column('standard_federal_region', String()),
-                      Column('census_region', String()),
-                      Column('census_region_name', String()),
-                      Column('census_division', String(),
-                             ForeignKey(DivLookup.census_division)),
-                      Column('census_division_name', String()),
-                      Column('circuit_court', String()),
-                      schema='demo', extend_existing=True)
+
+    __table__ = Table(
+        "state_fact",
+        Base.metadata,
+        Column("id", String()),
+        Column("name", String(), primary_key=True),
+        Column("abbreviation", String()),
+        Column("country", String()),
+        Column("type", String()),
+        Column("sort", String()),
+        Column("status", String()),
+        Column("occupied", String()),
+        Column("notes", String()),
+        Column("fips_state", String()),
+        Column("assoc_press", String()),
+        Column("standard_federal_region", String()),
+        Column("census_region", String()),
+        Column("census_region_name", String()),
+        Column("census_division", String(), ForeignKey(DivLookup.census_division)),
+        Column("census_division_name", String()),
+        Column("circuit_court", String()),
+        schema="demo",
+        extend_existing=True,
+    )
 
 
 class Census(Base):
@@ -78,21 +85,28 @@ class Census(Base):
 
     Again, a primary key is defined but will not be used.
     """
-    __table__ = Table('census', Base.metadata,
-                      Column('state', String(30), ForeignKey(StateFact.name),
-                             primary_key=True),
-                      Column('sex', String(1)),
-                      Column('age', Float()),
-                      Column('pop2000', Float()),
-                      Column('pop2008', Float()),
-                      schema='demo', extend_existing=True)
+
+    __table__ = Table(
+        "census",
+        Base.metadata,
+        Column("state", String(30), ForeignKey(StateFact.name), primary_key=True),
+        Column("sex", String(1)),
+        Column("age", Float()),
+        Column("pop2000", Float()),
+        Column("pop2008", Float()),
+        schema="demo",
+        extend_existing=True,
+    )
     state_fact = relationship(StateFact)
 
 
 # Another approach is to create a joined view
 class CensusJoined(Base):
-    __table__ = select([Census, StateFact]).select_from(
-        join(Census, StateFact, Census.state == StateFact.name)).alias()
+    __table__ = (
+        select([Census, StateFact])
+        .select_from(join(Census, StateFact, Census.state == StateFact.name))
+        .alias()
+    )
 
 
 # -------------
@@ -101,57 +115,79 @@ class CensusJoined(Base):
 # -------------
 
 
-
 class CensusService(RecipeServiceBaseV3):
     # Metrics are defined as an SQLAlchemy expression, and a label.
     metric_shelf = {
-        'pop2000': Metric(func.sum(Census.pop2000), label='Population 2000',
-                          format=".3s", singular="Population 2000",
-                          plural="Population 2000"),
-        'pop2008': Metric(func.sum(Census.pop2008), singular='Population 2008',
-                          format=".3s"),
-        'popdiff': Metric(func.sum(Census.pop2008 - Census.pop2000),
-                          format=".3s",
-                          singular='Population Growth'),
-        'avgage': Metric(
+        "pop2000": Metric(
+            func.sum(Census.pop2000),
+            label="Population 2000",
+            format=".3s",
+            singular="Population 2000",
+            plural="Population 2000",
+        ),
+        "pop2008": Metric(
+            func.sum(Census.pop2008), singular="Population 2008", format=".3s"
+        ),
+        "popdiff": Metric(
+            func.sum(Census.pop2008 - Census.pop2000),
+            format=".3s",
+            singular="Population Growth",
+        ),
+        "avgage": Metric(
             func.sum(Census.pop2008 * Census.age) / func.sum(Census.pop2008),
-            singular='Average Age', format=".1f"),
+            singular="Average Age",
+            format=".1f",
+        ),
         # A metric using a complex expression
-        'pctfemale': Metric(func.sum(
-            case([(Census.sex == 'F', Census.pop2008)], else_=0)) / func.sum(
-            Census.pop2008), format=".1%",
-                            singular='% Female'),
+        "pctfemale": Metric(
+            func.sum(case([(Census.sex == "F", Census.pop2008)], else_=0))
+            / func.sum(Census.pop2008),
+            format=".1%",
+            singular="% Female",
+        ),
         # a metric using a formatter
-        'pctdiff': Metric(func.sum(Census.pop2008 - Census.pop2000) / func.sum(
-            Census.pop2000),
-                          singular='Population Pct Change',
-                          # formatters=[
-                          #     lambda x: "Change is {0:0.1%} percent".format(
-                          #         x)]
-                              ),
+        "pctdiff": Metric(
+            func.sum(Census.pop2008 - Census.pop2000) / func.sum(Census.pop2000),
+            singular="Population Pct Change",
+            # formatters=[
+            #     lambda x: "Change is {0:0.1%} percent".format(
+            #         x)]
+        ),
     }
 
     # Dimensions are ways to split the data.
     dimension_shelf = {
         # Simplest possible dimension, a SQLAlchemy expression and a label.
-        'state': Dimension(Census.state, singular='State', plural='States',
-                           format=""),
-        'first_letter_state': Dimension(func.substring(Census.state, 1, 1),
-                                        label='State'),
-        'age': Dimension(Census.age, singular='Age', plural='Ages'),
-        'age_bands': Dimension(case([(Census.age < 21, 'Under 21'),
-                                     (Census.age < 49, '21-49')
-                                     ], else_='Other'), label='Age Bands'),
+        "state": Dimension(Census.state, singular="State", plural="States", format=""),
+        "first_letter_state": Dimension(
+            func.substring(Census.state, 1, 1), label="State"
+        ),
+        "age": Dimension(Census.age, singular="Age", plural="Ages"),
+        "age_bands": Dimension(
+            case(
+                [(Census.age < 21, "Under 21"), (Census.age < 49, "21-49")],
+                else_="Other",
+            ),
+            label="Age Bands",
+        ),
         # This will use the lookup to get display values of "M" and "F"
-        'sex': LookupDimension(Census.sex, singular='Gender', plural='Genders',
-                               lookup={'M': 'Menfolk', "F": "Womenfolk"}),
+        "sex": LookupDimension(
+            Census.sex,
+            singular="Gender",
+            plural="Genders",
+            lookup={"M": "Menfolk", "F": "Womenfolk"},
+        ),
         # Formatters apply functions to the response
-        'gender': Dimension(Census.sex, label='Sex',
-                            formatters=[lambda x: ord(x), lambda x: x + 100]),
+        "gender": Dimension(
+            Census.sex, label="Sex", formatters=[lambda x: ord(x), lambda x: x + 100]
+        ),
     }
 
     # Automatic filter keys are used for the global filters as well
-    automatic_filter_keys = ('sex', 'state',)
+    automatic_filter_keys = (
+        "sex",
+        "state",
+    )
 
     def __init__(self, *args, **kwargs):
         super(CensusService, self).__init__(*args, **kwargs)
@@ -162,22 +198,23 @@ class CensusService(RecipeServiceBaseV3):
 # Step 3) The data services response.
 # -------------
 
+
 class FilterService(CensusService):
     def build_response(self):
-        self.metrics = ('pop2000', )
+        self.metrics = ("pop2000",)
         recipes = []
         for dim in self.automatic_filter_keys:
             recipe = self.recipe().metrics(*self.metrics).dimensions(dim)
             recipes.append((recipe, dim))
 
         results = RecipePool(recipes).run()
-        self.response['responses'] = results
+        self.response["responses"] = results
 
 
 class FirstChooserV3Service(CensusService):
     def build_recipe(self):
         start = current_milli_time()
-        self.metrics = ('pop2000', 'pop2008', 'popdiff', 'avgage', 'pctfemale')
-        recipe = self.recipe().metrics(*self.metrics).prepare(flavor='metric')
-        print('Ms: ', current_milli_time() - start)
+        self.metrics = ("pop2000", "pop2008", "popdiff", "avgage", "pctfemale")
+        recipe = self.recipe().metrics(*self.metrics).prepare(flavor="metric")
+        print("Ms: ", current_milli_time() - start)
         return [recipe]
